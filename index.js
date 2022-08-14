243c5983518344666ca0f9365185f872c922c270
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-08-14 17:57:55 +0630

    add register route

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
+
 dotenv.config();
 const app = express();
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB connection successfully!"))
   .catch((err) => console.log(err));
 
-app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 6000, () =>
   console.log(`Backend server is running on port ${process.env.PORT}`)
--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,20 @@
+const User = require("../models/User");
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
+
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password: CryptoJS.AES.encrypt(password, process.env.PASS_SEC).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
