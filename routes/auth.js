173230a78a62e1e09a8e18159b7675ec5a44878e
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-08-21 10:19:29 +0630

    add jwt in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
@@ -30,8 +31,18 @@ router.post("/login", async (req, res) => {
     if (originalPassword !== req.body.password) {
       return res.status(401).json("Wrong credentials!");
     }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
