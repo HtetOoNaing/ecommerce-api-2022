358bc351dc04330a3f419a83da92d186ab79f76e
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-08-23 00:26:39 +0630

    add product route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 6000, () =>
   console.log(`Backend server is running on port ${process.env.PORT}`)
